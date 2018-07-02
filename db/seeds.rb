d0eeee6c9fd8b6c4d8391b301a7495f37d89c31a
Jeni Brandt <jenibrandt@legalshieldcorp.com>
2018-07-02 16:39:57 -0500

    create unique post and comment

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,6 +16,16 @@ posts = Post.all
     )
 end
 
+myPost = Post.find_or_create_by!(
+    title: "My unique post title",
+    body: "My unique post has a unique body!"
+)
+
+Comment.find_or_create_by!(
+    post: myPost,
+    body: "This seeding stuff is confusing me more than it should!"
+)
+
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
