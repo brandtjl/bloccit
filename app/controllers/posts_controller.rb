d6559be713697905e8f8cd969ae5a51145c542de
Jeni Brandt <jenibrandt@legalshieldcorp.com>
2018-07-05 15:18:53 -0500

    mark SPAM entries

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,11 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.all  # why is this Post.all and not Posts.all? in 2018.._create_posts.rb it says 'create_table posts'
+    @posts.each_with_index do |post, index|
+      if index % 5 == 0
+        post.title = 'SPAM'
+      end 
+    end 
   end
 
   def show
