78c880dd1145e645c047e62c4f92d20ff2f95446
Jeni Brandt <jenibrandt@legalshieldcorp.com>
2018-07-13 14:56:57 -0500

    update and delete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,5 +27,35 @@ class PostsController < ApplicationController
   end 
 
   def edit
+    @post = Post.find(params[:id])
   end
+
+  def update
+    @post = Post.find(params[:id])  #is this setting @post to the original values of the post? 
+     @post.title = params[:post][:title] # and is this changing the value?
+     @post.body = params[:post][:body]
+ 
+     if @post.save
+       flash[:notice] = "Post was updated."
+       redirect_to @post
+     else
+       flash.now[:alert] = "There was an error saving the post. Please try again."
+       render :edit
+     end
+
+  end
+
+  def destroy
+      @post = Post.find(params[:id])
+  
+      if @post.destroy
+        flash[:notice] = "\"#{@post.title}\" was deleted successfully."
+        redirect_to posts_path
+      else
+        flash.now[:alert] = "There was an error deleting the post."
+        render :show
+      end
+   
+  end
+
 end
