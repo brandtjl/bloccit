1f2bbbf16bcba27163b66503e8bdab2e556d6808
Jeni Brandt <jenibrandt@legalshieldcorp.com>
2018-07-27 16:31:47 -0500

    add validations to topics

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -7,7 +7,14 @@ RSpec.describe Topic, type: :model do
    let(:topic) { Topic.create!(name: name, description: description) }
 
    it { is_expected.to have_many(:posts) }
- # #1
+
+   it { is_expected.to validate_presence_of(:name) }
+   it { is_expected.to validate_presence_of(:description) }
+ 
+   it { is_expected.to validate_length_of(:name).is_at_least(5) }
+   it { is_expected.to validate_length_of(:description).is_at_least(15) }
+ 
+   # #1
    describe "attributes" do
      it "has name, description, and public attributes" do
        expect(topic).to have_attributes(name: name, description: description, public: public)
