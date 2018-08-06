07645a6daee3166934dbc71f437cefe2021a66b9
Jeni Brandt <jenibrandt@legalshieldcorp.com>
2018-08-06 14:58:59 -0500

    create User model and specs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_19_201113) do
+ActiveRecord::Schema.define(version: 2018_08_06_183538) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string "title"
@@ -45,10 +45,28 @@ ActiveRecord::Schema.define(version: 2018_07_19_201113) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "sponsored_posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "price"
+    t.integer "topic_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["topic_id"], name: "index_sponsored_posts_on_topic_id"
+  end
+
   create_table "topics", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.boolean "public", default: true
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
