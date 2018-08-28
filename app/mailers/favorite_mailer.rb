d73480c046d150f82071ddd487b151488a6bfec7
Jeni Brandt <jenibrandt@legalshieldcorp.com>
2018-08-28 16:16:47 -0500

    Favorites assignment

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -2,17 +2,28 @@ class FavoriteMailer < ApplicationMailer
     default from: "jenibrandt@legalshieldcorp.com"
 
     def new_comment(user, post, comment)
- 
-        # #18
-            headers["Message-ID"] = "<comments/#{comment.id}@your-app-name.example>"
-            headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
-            headers["References"] = "<post/#{post.id}@your-app-name.example>"
-        
-            @user = user
-            @post = post
-            @comment = comment
-        
-        # #19
-            mail(to: user.email, subject: "New comment on #{post.title}")
-          end
+        #new_comment has a matching view that is 'envoked'?
+# #18
+        headers["Message-ID"] = "<comments/#{comment.id}@your-app-name.example>"
+        headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
+        headers["References"] = "<post/#{post.id}@your-app-name.example>"
+
+        @user = user
+        @post = post
+        @comment = comment
+
+    # #19
+        mail(to: user.email, subject: "New comment on #{post.title}")
+    end
+
+    def new_post(post)
+        headers["Message-ID"] = "<posts/#{post.id}@your-app-name.example>"
+        headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
+        headers["References"] = "<post/#{post.id}@your-app-name.example>"
+            #why is one of the above 'posts' and the others 'post'?
+        @post = post
+        mail(to: post.user.email, subject: "Favorited post #{post.title}")
+
+    end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,13 +5,15 @@ class Post < ApplicationRecord
     has_many :comments, dependent: :destroy
     has_many :votes, dependent: :destroy
     has_many :favorites, dependent: :destroy
-    
+     
     default_scope { order('rank DESC') }
     
     validates :title, length: { minimum: 5 }, presence: true
    validates :body, length: { minimum: 20 }, presence: true
    validates :topic, presence: true
    validates :user, presence: true
+
+   after_create :auto_favorite
 
    def up_votes
     # #9
@@ -33,5 +35,12 @@ class Post < ApplicationRecord
         age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
         new_rank = points + age_in_days
         update_attribute(:rank, new_rank)
+    end
+
+    def auto_favorite
+        Favorite.create(post: self, user: self.user)
+        # why not Favorites.build(post: post) ? or use current_user?
+        FavoriteMailer.new_post(self).deliver_now
+            #new_post is method in favorite_mailer
     end
 end
