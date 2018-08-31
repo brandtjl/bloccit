07d87c6cb460edf14ab4d0cb05f4f104d4d44087
Jeni Brandt <jenibrandt@legalshieldcorp.com>
2018-08-31 13:18:39 -0500

    attempting Favorites assignment again

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -5,14 +5,30 @@ class FavoriteMailer < ApplicationMailer
  
         # #18
             headers["Message-ID"] = "<comments/#{comment.id}@your-app-name.example>"
+            # not sure why plural 'comments'(above) but singular 'post' (below)
             headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
             headers["References"] = "<post/#{post.id}@your-app-name.example>"
         
             @user = user
             @post = post
             @comment = comment
-        
+        #why are @user, @post, and @comment necessary? 
+
         # #19
             mail(to: user.email, subject: "New comment on #{post.title}")
+            
           end
+
+    def new_post(user, post)
+        headers["Message-ID"] = "<posts/#{comment.id}@your-app-name.example>"
+        headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
+        headers["References"] = "<post/#{post.id}@your-app-name.example>"
+    
+        @user = user
+        @post = post
+    
+    # #19
+        mail(to: user.email, subject: "New favorited post: #{post.title}")
+    end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -13,6 +13,8 @@ class Post < ApplicationRecord
    validates :topic, presence: true
    validates :user, presence: true
 
+   after_create :auto_favorite
+
    def up_votes
     # #9
         votes.where(value: 1).count
@@ -33,5 +35,12 @@ class Post < ApplicationRecord
         age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
         new_rank = points + age_in_days
         update_attribute(:rank, new_rank)
+    end
+
+    def auto_favorite
+        Favorite.create(post: self, user: self.user)
+        # when doing an action on the instance that's calling the method (in a model), use self
+        # post = self because it's in the Post model and this action is happening after a Post is created
+        
     end
 end
